18bdd3c0827e791bf98b4c1384eb36a81b9c8f98
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2023-06-11 19:22:50 +0200

    fix: workspace entity migration

--- packages/backend/src/app/database/seeders/@_create-workspace.ts
+++ packages/backend/src/app/database/seeders/@_create-workspace.ts
@@ -5,25 +5,22 @@ module.exports = {
     await queryInterface.bulkInsert('workspaces', [
       {
         name: 'Workspace 1',
-        closed: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Workspace 2',
-        closed: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Workspace 3',
-        closed: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
     ]);
   },
   async down(queryInterface) {
-    await queryInterface.bulkDelete('boards');
+    await queryInterface.bulkDelete('workspaces');
   },
 };
