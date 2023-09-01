a7ce16f55cf9c0876d36fce9e87c4bb7170bb3fb
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2023-09-01 22:09:16 +0200

    feature: added base libs structure

--- packages/frontend/src/app/components/Dashboard/index.tsx
+++ packages/frontend/src/app/components/Dashboard/index.tsx
@@ -1,80 +1,16 @@
 import * as React from 'react';
 
 import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 
-import { mainListItems, secondaryListItems } from './listItems';
-import { AppBar, Drawer, Toolbar } from './styled';
+import { SideNav } from '@pmt/main/features/side-nav';
+import { Toolbar } from './styled';
 
 export const Dashboard = () => {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
-
   return (
     <Box sx={{ display: 'flex' }}>
-      <AppBar position="absolute" open={open}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={toggleDrawer}
-            sx={{
-              marginRight: '36px',
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            sx={{ flexGrow: 1 }}
-          >
-            Dashboard name
-          </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <Toolbar>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            sx={{ flexGrow: 1 }}
-          >
-            Workspace name
-          </Typography>
-          <IconButton onClick={toggleDrawer}>
-            <ChevronLeftIcon color="inherit" />
-          </IconButton>
-        </Toolbar>
-        <Divider />
-        <List component="nav">
-          {mainListItems}
-          <Divider sx={{ my: 1 }} />
-          {secondaryListItems}
-        </List>
-      </Drawer>
+      <SideNav />
       <Box
         component="main"
         sx={{
